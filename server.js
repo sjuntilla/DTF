105b25727d45e85c94af7b79a079e162684f5074
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-03-30 10:02:55 -1000

    servers updated

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ server.listen(8080);
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
+
+io.on('connection', function (socket) {
+    socket.emit('FACK', { hello: 'friends' });
+    socket.on('SCRAP', function (data) {
+        console.log(data);
+    });
+
+    socket.on('fromClient', function (data) {
+        console.log('data from client', data);
+        socket.broadcast.emit('FACK', data);
+    });
+});
