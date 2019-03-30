c852730fd9c88b714d3f52d6b6634ef5470d0ffc
Sam <juntilla@hawaii.edu>
2019-03-30 12:01:50 -1000

    adds knexfile

--- database/knex.js
+++ database/knex.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-const config = require("../knexfile");
-module.exports = require("knex")(config);
-=======
 const config = require('../knexfile');
 module.exports = require('knex')(config);
->>>>>>> 8f9290914b435eb11e48f4bd9bdd386c315bb0b1
