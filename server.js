542126d4a9becb417216e0da84161b328cfdad1b
Sam <juntilla@hawaii.edu>
2019-03-30 09:53:12 -1000

    begins server setup

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const io = require('socket.io')(server);
 const app = require('express')();
 const server = require('http').Server(app);
+
+server.listen(8080);
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
